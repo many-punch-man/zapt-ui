205c4f37a5a0abeae5aaf81ecdbf26afd05664eb
Qiksy <brianjohnsonp0i631@gmail.com>
2024-08-28 00:24:43 +0800

    去掉unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,6 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
     modules: [
-        '@unocss/nuxt',
         '@element-plus/nuxt'
     ],
     elementPlus: {
