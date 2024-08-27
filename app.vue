bfbcdb470d95e32a15ae02a28154b271203e21ba
Qiksy <brianjohnsonp0i631@gmail.com>
2024-08-27 21:20:44 +0800

    暂存

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-#000000 w-full h-full">
-    <el-button class="pt-200px">
-      123456
-    </el-button>
+  <div>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    这里是首页
+  </div>
+</template>
+
+<script lang="tsx" setup>
+defineOptions({name: 'index'})
+</script>
