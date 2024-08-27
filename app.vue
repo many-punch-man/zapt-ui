7d59f992775365dcced02d6be60ae0e58c7707d6
Qiksy <brianjohnsonp0i631@gmail.com>
2024-08-27 18:09:15 +0800

    集成unocss

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <el-button>
+  <div class="bg-#000000 w-full h-full">
+    <el-button class="pt-200px">
       123456
     </el-button>
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,22 +2,25 @@
 import {include, exclude} from "./build/optimize";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ['@element-plus/nuxt'],
-  elementPlus: {
-    importStyle: 'scss',
-  },
-  css: ['~/assets/scss/index.scss'],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `@use "@/assets/scss/element/index.scss" as element;`,
-          quietDeps: true
-        }
-      }
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    modules: [
+        '@unocss/nuxt',
+        '@element-plus/nuxt'
+    ],
+    elementPlus: {
+        importStyle: 'scss',
     },
-    optimizeDeps: {include, exclude}
-  },
+    css: ['~/assets/scss/index.scss'],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@use "@/assets/scss/element/index.scss" as element;`,
+                    quietDeps: true
+                }
+            }
+        },
+        optimizeDeps: {include, exclude}
+    },
 })
