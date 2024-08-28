41c5a0432c3daf73410f820695403ded3ee5ba74
Qiksy <brianjohnsonp0i631@gmail.com>
2024-08-28 16:54:35 +0800

    引入滚动 的动画库

--- app.vue
+++ app.vue
@@ -5,3 +5,16 @@
     </NuxtLayout>
   </div>
 </template>
+
+
+<script setup>
+import AOS from 'aos'
+onMounted(()=>{
+  AOS.init({
+    once: true,
+    disable: 'phone',
+    duration: 600,
+    easing: 'ease-out-sine',
+  })
+})
+</script>
