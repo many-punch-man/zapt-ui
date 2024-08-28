4003073e6eb5d382284c305ae79b60dd4e08707a
Qiksy <brianjohnsonp0i631@gmail.com>
2024-08-28 15:07:12 +0800

    header优化

--- components/Header.vue
+++ components/Header.vue
@@ -68,7 +68,66 @@
 
         <!-- 移动端导航栏 -->
         <div class="md:hidden">
-          222
+
+          <!-- Hamburger button -->
+          <button class="hamburger" ref="hamburger" :class="{ active: mobileNavOpen }" aria-controls="mobile-nav"
+                  :aria-expanded="mobileNavOpen" @click="mobileNavOpen = !mobileNavOpen">
+            <span class="sr-only">Menu</span>
+            <svg class="w-6 h-6 fill-current text-gray-300 hover:text-gray-200 transition duration-150 ease-in-out"
+                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <rect y="4" width="24" height="2" rx="1" />
+              <rect y="11" width="24" height="2" rx="1" />
+              <rect y="18" width="24" height="2" rx="1" />
+            </svg>
+          </button>
+
+          <!-- Mobile navigation -->
+          <nav id="mobile-nav" ref="mobileNav"
+               class="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out"
+               :style="[mobileNavOpen ? { maxHeight: mobileNav.scrollHeight + 'px', opacity: 1 } : { maxHeight: 0, opacity: .8 }]">
+            <ul class="bg-gray-800 px-4 py-2">
+              <li>
+                <router-link to="/features" class="flex text-gray-300 hover:text-gray-200 py-2">Features</router-link>
+              </li>
+              <li>
+                <router-link to="/pricing" class="flex text-gray-300 hover:text-gray-200 py-2">Pricing</router-link>
+              </li>
+              <li>
+                <router-link to="/blog" class="flex text-gray-300 hover:text-gray-200 py-2">Blog</router-link>
+              </li>
+              <li>
+                <router-link to="/about" class="flex text-gray-300 hover:text-gray-200 py-2">About us</router-link>
+              </li>
+              <li class="py-2 my-2 border-t border-b border-gray-700">
+                <span class="flex text-gray-300 py-2">Support</span>
+                <ul class="pl-4">
+                  <li>
+                    <router-link to="/contact"
+                                 class="text-sm flex font-medium text-gray-400 hover:text-gray-200 py-2">Contact us</router-link>
+                  </li>
+                  <li>
+                    <router-link to="/help" class="text-sm flex font-medium text-gray-400 hover:text-gray-200 py-2">Help
+                      center</router-link>
+                  </li>
+                  <li>
+                    <router-link to="/404"
+                                 class="text-sm flex font-medium text-gray-400 hover:text-gray-200 py-2">404</router-link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <router-link to="/signin"
+                             class="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center">Sign
+                  in</router-link>
+              </li>
+              <li>
+                <router-link to="/signup"
+                             class="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out">Sign
+                  up</router-link>
+              </li>
+            </ul>
+          </nav>
+
         </div>
 
 
@@ -83,4 +142,28 @@
 import {ref} from 'vue'
 
 defineOptions({name: 'Header'})
+
+
+const mobileNavOpen = ref(false)
+const mobileNav = ref()
+const hamburger = ref()
+const clickOutside = (e: MouseEvent) => {
+  if(!mobileNavOpen.value || mobileNav.value.contains(e.target) || hamburger.value.contains(e.target)) return
+  mobileNavOpen.value = false
+}
+
+const keyPress = (e:KeyboardEvent) => {
+  if(!mobileNavOpen.value || e.code!=='27') return
+  mobileNavOpen.value = false
+}
+
+onMounted(() => {
+  document.addEventListener('click', clickOutside)
+  document.addEventListener('keydown', keyPress)
+})
+
+onBeforeUnmount(() => {
+  document.removeEventListener('click', clickOutside)
+  document.removeEventListener('keydown', keyPress)
+})
 </script>
