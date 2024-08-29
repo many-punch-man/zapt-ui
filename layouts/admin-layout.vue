7b38ebbdf66aa5e5dd843cfe804ea549069415ed
Qiksy <brianjohnsonp0i631@gmail.com>
2024-08-29 11:43:47 +0800

    全局黑暗模式

--- layouts/admin-layout.vue
+++ layouts/admin-layout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex h-[100dvh] overflow-hidden">
+  <div class="flex h-[100dvh] overflow-hidden font-inter antialiased bg-gray-100 dark:bg-gray-900 text-gray-600 dark:text-gray-400">
     <!-- Sidebar -->
     <AdminSidebar :sidebarOpen="sidebarOpen" @close-sidebar="sidebarOpen = false" />
 
