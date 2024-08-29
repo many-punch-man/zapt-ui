83d224501a91bc9ab35e28fcd53c40e0db8d9427
Qiksy <brianjohnsonp0i631@gmail.com>
2024-08-29 10:33:34 +0800

    侧边栏完善

--- components/admin/SidebarLinkGroup.vue
+++ components/admin/SidebarLinkGroup.vue
@@ -1,5 +1,5 @@
 <template>
-  <li class="px-3 py-2 rounded-sm mb-0.5 last:mb-0" :class="activeCondition && 'bg-slate-900'">
+  <li class="px-3 py-2 rounded-sm mb-0.5 last:mb-0 list-none" :class="activeCondition && 'bg-slate-900'">
     <slot :handleClick="handleClick" :expanded="expanded" />
   </li>
 </template>
