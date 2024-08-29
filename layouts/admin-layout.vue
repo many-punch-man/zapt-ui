5d8748380e6d5a7506feb32cd88d3f28da0cf73c
Qiksy <brianjohnsonp0i631@gmail.com>
2024-08-29 14:12:04 +0800

    样式修改

--- layouts/admin-layout.vue
+++ layouts/admin-layout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex h-[100dvh] overflow-hidden font-inter antialiased bg-gray-100 dark:bg-gray-900 text-gray-600 dark:text-gray-400">
+  <div class="flex h-[100dvh] overflow-hidden font-inter antialiased bg-gray-100 dark:bg-gray-900 text-gray-600 dark:text-gray-400 transition-all duration-200 ease-in-out">
     <!-- Sidebar -->
     <AdminSidebar :sidebarOpen="sidebarOpen" @close-sidebar="sidebarOpen = false" />
 
