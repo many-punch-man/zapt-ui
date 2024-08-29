6382024301fdf62c17e3a4acf867910fc9484983
Qiksy <brianjohnsonp0i631@gmail.com>
2024-08-29 13:49:13 +0800

    样式修改

--- layouts/admin-layout.vue
+++ layouts/admin-layout.vue
@@ -6,7 +6,7 @@
     <!-- Content area -->
     <div class="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
       <!-- Site header -->
-      <AdminHeader :sidebarOpen="sidebarOpen" @toggle-sidebar="sidebarOpen = !sidebarOpen" />
+      <AdminHeader :sidebarOpen="sidebarOpen" @toggle-sidebar="sidebarOpen = !sidebarOpen" variant="v1" />
       <main class="grow">
         <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
             <slot></slot>
