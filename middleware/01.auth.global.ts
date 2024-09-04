dc6d229232a395d1547f96e47d0578537ee2f0e7
Qiksy <brianjohnsonp0i631@gmail.com>
2024-09-04 16:20:03 +0800

    路由校验

--- middleware/01.auth.global.ts
+++ middleware/01.auth.global.ts
@@ -8,34 +8,31 @@ const whiteList = [
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
     if (import.meta.client) {
+        console.log("客户端校验")
+        const userToken = useCookie<UserToken>('userToken')
+
         //客户端进行校验
         if (to.fullPath.match(/^\/admin.*$/)) {
             // 这里是管理员端口
-            const userToken = useCookie<UserToken>('userToken')
-            console.log(to.fullPath)
+
+            console.log("这里是管理员端口")
 
             if (userToken.value?.accessToken) {
-                if (to.path === '/signin') {
-                    // 已经登录的，直接返回去管理员首页，不要重复登录
-                    console.log("middleware: 已经登录，重定向到首页")
-                    return navigateTo('/admin')
+                // 从localStorage中加载用户信息
+                useUserStore().loadState();
+
+                if (!useUserStore().getIsSetUser) {
+                    // 如果pinia中，没有用户信息，则请求接口获取用户信息
+                    await useUserStore().setUserInfoAction()
+
+                    //获取后台赋予的权限列表
+                    const permissionList = useUserStore().getMenus
+                    // todo 后续校验路由权限
+
+                    return
                 } else {
-                    // 从localStorage中加载用户信息
-                    useUserStore().loadState();
-
-                    if (!useUserStore().getIsSetUser) {
-                        // 如果pinia中，没有用户信息，则请求接口获取用户信息
-                        await useUserStore().setUserInfoAction()
-
-                        //获取后台赋予的权限列表
-                        const permissionList = useUserStore().getMenus
-                        // todo 后续校验路由权限
-
-                        return
-                    }else {
-                        //todo 后续校验路由权限
-                        return
-                    }
+                    //todo 后续校验路由权限
+                    return
                 }
             } else {
                 // 如果访问token不存在，则直接跳到登录页
@@ -43,6 +40,12 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
                 return navigateTo('/signin')
             }
 
+        } else if (to.fullPath === '/signin') {
+            if (userToken.value?.accessToken) {
+                console.log("middleware: accessToken存在，重定向")
+                return navigateTo("/admin")
+            }
+            return
         }
     }
 })
